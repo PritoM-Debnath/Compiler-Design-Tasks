73f01b49bd520f4cfc8aa4ea175f4d6eae188683
PritoM-Debnath <94226214+PritoM-Debnath@users.noreply.github.com>
2021-11-14 07:58:23 +0600

    First task in lab
    This will take input (Code with comments ) from the input.txt file and generate output in two ways. One in files output.txt( all the comments ) and the console ( the code without comments)

---
+++ CD Task-1.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    
+    string s="";
+    int count=0;
+
+
+    vector<string>comment;
+    
+    while(1){
+    	string t;
+		getline(cin, t);
+
+		if(t[0]=='/'){
+			comment.push_back(t);
+    	}
+    	else if(t[t.size()-1]=='/'){
+			comment.push_back(t);
+		}
+    	else{
+			s+=t;
+			s+="\n";
+		}
+		if(t.find('{')<t.size()){
+			count++;
+		}
+    	if(t.find('}')<t.size()){
+    		count--;
+		}
+		if(t.find('}')<t.size() and count==0){
+			break;
+		}
+	}
+    
+
+    for(auto u:comment){
+    cout<< u << endl;
+    }
+
+    cerr<< s << endl;
+return 0;
+}
